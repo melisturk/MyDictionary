95ed7af9bff44406d04021a3c9b0959ac5eb05c5
Melis Türk <melis-trk@outlook.com>
2021-02-15 22:18:29 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T>
+    {
+        T[] _array;
+        T[] tempArray;
+
+        public MyDictionary()
+        {
+            _array = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                _array[i] = tempArray[i];
+            }
+
+            _array[_array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+    }
+}
